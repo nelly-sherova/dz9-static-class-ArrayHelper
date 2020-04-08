0d28ad16d0935f0644faf1e60435e777ee173e8e
unknown <lvova.nikol@mail.ru>
2020-04-08 14:24:13 +0300

    Добавление метода Push в наш класс и проверка его работы в Main

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace Array_Helper
     {
         static void Main(string[] args)
         {
-            
+            float[] arr = {1.3f,2.5f,3.9f,4.0009f};
+            for (int i = 0; i < arr.Length; i++) Console.Write($"{arr[i]}  ");
+            Console.WriteLine(ArrayHelper.Push(ref arr, 7.009f) );
+            for (int i = 0; i < arr.Length; i++) Console.Write($"{arr[i]}  ");
         }
     }
     static class ArrayHelper
@@ -40,6 +43,36 @@ namespace Array_Helper
             float n = arr[arr.Length-1];
             Array.Resize(ref arr, arr.Length-1);
             return n;
+        }
+        public static int Push(ref int[] arr, int var)
+        {
+            Array.Resize(ref arr, arr.Length+1);
+            arr[arr.Length-1] = var;
+            return arr.Length;
+        }
+        public static string Push(ref string[] arr, string var)
+        {
+            Array.Resize(ref arr, arr.Length+1);
+            arr[arr.Length-1] = var;
+            return Convert.ToString(arr.Length);
+        }
+        public static double Push(ref double[] arr, double var)
+        {
+            Array.Resize(ref arr, arr.Length+1);
+            arr[arr.Length-1] = var;
+            return arr.Length;
+        }
+        public static decimal Push(ref decimal[] arr, decimal var)
+        {
+            Array.Resize(ref arr, arr.Length+1);
+            arr[arr.Length-1] = var;
+            return arr.Length;
+        }
+        public static float Push(ref float[] arr, float var)
+        {
+            Array.Resize(ref arr, arr.Length+1);
+            arr[arr.Length-1] = var;
+            return arr.Length;
         }
     }
 }
