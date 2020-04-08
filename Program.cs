357fd3d68e9e56aae71452e0ad41cf18b4a5d416
unknown <lvova.nikol@mail.ru>
2020-04-08 14:22:16 +0300

    Объявление класса ArrayHelper, добавление в него  метода Pop, с перегрузками

--- Program.cs
+++ Program.cs
@@ -6,7 +6,40 @@ namespace Array_Helper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+    }
+    static class ArrayHelper
+    {
+        public static int Pop(ref int[] arr)
+        {
+            int n = arr[arr.Length-1];
+            Array.Resize(ref arr, arr.Length-1);
+            return n;
+        }
+        public static string Pop(ref string[] arr)
+        {
+            string n = arr[arr.Length-1];
+            Array.Resize(ref arr, arr.Length-1);
+            return n;
+        }
+        public static double Pop(ref double[] arr)
+        {
+            double n = arr[arr.Length-1];
+            Array.Resize(ref arr, arr.Length-1);
+            return n;
+        }
+        public static decimal Pop(ref decimal[] arr)
+        {
+            decimal n = arr[arr.Length-1];
+            Array.Resize(ref arr, arr.Length-1);
+            return n;
+        }
+        public static float Pop(ref float[] arr)
+        {
+            float n = arr[arr.Length-1];
+            Array.Resize(ref arr, arr.Length-1);
+            return n;
         }
     }
 }
