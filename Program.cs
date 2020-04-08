b855adeca5ddb9cedfe56bbe886ef8a74bc0b047
unknown <lvova.nikol@mail.ru>
2020-04-08 14:18:38 +0300

    Начало работы

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Array_Helper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
